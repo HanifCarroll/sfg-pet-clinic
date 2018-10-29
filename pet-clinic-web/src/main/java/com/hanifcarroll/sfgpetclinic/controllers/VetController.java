3828e39ea3a34ba5e7fe0c03f2527023437425ce
Hanif Carroll <hanifcarroll@gmail.com>
2018-10-29 19:21:48 -0400

    Add vets page and controller.

---
+++ pet-clinic-web/src/main/java/com/hanifcarroll/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,16 @@
+package com.hanifcarroll.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "vets/index";
+    }
+}
