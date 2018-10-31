491cd077c3644d1e126433d9d9adebc62cb48129
Hanif Carroll <hanifcarroll@gmail.com>
2018-10-31 12:41:48 -0400

    Add specialties entity.  Add specialties to Vet.

--- pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package com.hanifcarroll.sfgpetclinic.model;
 
 import javax.persistence.Entity;
+import java.util.Set;
 
 @Entity
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
