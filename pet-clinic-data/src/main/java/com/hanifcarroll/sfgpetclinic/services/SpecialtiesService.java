1a0b272b9f266b1e7df8d9b8abac5339b9d1f59f
Hanif Carroll <hanifcarroll@gmail.com>
2018-10-31 13:37:36 -0400

    Add specialties map impl.

---
+++ pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.hanifcarroll.sfgpetclinic.services;
+
+import com.hanifcarroll.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
