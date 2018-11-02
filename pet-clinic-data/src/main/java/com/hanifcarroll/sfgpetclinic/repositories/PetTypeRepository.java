024263cc2a426403020934f1017411f2bf3c0f9a
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-02 11:09:51 -0400

    Add repositories.

---
+++ pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/repositories/PetTypeRepository.java
@@ -0,0 +1,7 @@
+package com.hanifcarroll.sfgpetclinic.repositories;
+
+import com.hanifcarroll.sfgpetclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/repositories/VisitRepository.java
@@ -0,0 +1,7 @@
+package com.hanifcarroll.sfgpetclinic.repositories;
+
+import com.hanifcarroll.sfgpetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
