f2640c6946b01b437cf38921d67dd4d637442711
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-02 10:50:39 -0400

    Update Vet to use JPA

--- pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/hanifcarroll/sfgpetclinic/model/Specialty.java
@@ -1,8 +1,15 @@
 package com.hanifcarroll.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
-    public String description;
+    @Column(name = "description")
+    private String description;
 
     public String getDescription() {
         return description;
